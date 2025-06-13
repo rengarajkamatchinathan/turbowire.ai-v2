24c379e91afe6b24244baa0b83e50d71d9edac20
Rengaraj Kamatchinathan <Rengaraj.k@synergech.com>
2025-06-13 16:06:01 +0530

    removed subs

--- app/(main)/pricing/page.jsx
+++ app/(main)/pricing/page.jsx
@@ -10,9 +10,7 @@ function Pricing() {
   return (
     <div className="mt-20 flex flex-col space-y-6 items-center p-10 md:px-32 lg:px-48 w-full ">
       <div className="flex items-center gap-3 mb-4">
-        <Zap className="w-8 h-8 text-blue-400 neon-text-blue" />
         <h2 className="font-bold text-5xl hologram-text">PRICING</h2>
-        <Cpu className="w-6 h-6 text-orange-400 neon-text-orange" />
       </div>
       <p className="text-gray-400 max-w-xl text-center mt-4">
         {Lookup.PRICING_DESC}
@@ -22,7 +20,7 @@ function Pricing() {
           <span className="font-bold neon-text-blue">{userDetail?.token}</span> Tokens Left
         </h2>
       </div>
-      <PricingModel />
+      {/* <PricingModel /> */}
     </div>
   );
 }
